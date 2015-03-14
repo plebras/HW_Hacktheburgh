00f52f6a095a861d953a7b159b6133fcf06d7ba7
Tamara Tanasescu <gokimster@yahoo.com>
2015-03-14 19:43:55 +0000

    Start on tile puzzle

---
+++ Assets/FloorTile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
