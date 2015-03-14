5c073741417db6da548e6a9a3f621444926deedd
Tamara Tanasescu <gokimster@yahoo.com>
2015-03-14 23:19:46 +0000

    MLH Tile puzzle

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTile : MonoBehaviour {
+
+	int color = 0;
+	public int correctColor = 0;
+	// Use this for initialization
+	void Start () {
+		SetColor ();
+
+	}
+
+	void SetColor()
+	{
+		if(color > 2)
+			color = 0;
+		if(color == 0)
+			gameObject.renderer.material.color = new Color(0.86f,0.3f,0f,1f);
+		if(color == 1)
+			gameObject.renderer.material.color = new Color(0.1f,0.15f,0.48f,1f);
+		if(color == 2)
+			gameObject.renderer.material.color = new Color(0.91f,0.8f,0.18f,1f) ;
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("COLLIDING");
+		if (collision.collider.CompareTag("Player")){
+			Debug.Log("COLLIDING WithPlayer");
+			ChangeColor();
+		}
+	}
+
+	void ChangeColor(){
+		color++;
+		SetColor ();
+	}
+	
+	public bool IsCorrectColor(){
+		return (color == correctColor);
+	}
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	public Transform []tiles = new Transform[100];
+	public FloorTile tileScript;
+	void Start () {
+		for(int i = 0; i<transform.childCount; i++)
+		{
+			tiles[i] =  (Transform) transform.GetChild(i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool solved = true;
+		for(int i = 0; i<transform.childCount; i++)
+		{
+			tileScript = tiles[i].gameObject.GetComponent<FloorTile>();
+			if (!tileScript.IsCorrectColor())
+				solved = false;
+		}
+		if (solved)
+			Destroy(transform.parent.gameObject);
+	}
+}
